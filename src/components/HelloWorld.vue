ca4d73ff76c3c270ab7db347bac314dc3aa78a0d
Marvin Qian <fueledbymarvin@gmail.com>
2021-12-21 22:59:07 -0800

    vuex smart module

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,119 +1,57 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div>{{ greeting("Looped") }}</div>
+    <button @click="register">Register foo</button>
+    <div v-for="[k, v] in foos" :key="k">
+      <div>Foo {{ k }}: {{ v }}</div>
+      <button @click="increment(k)">Increment</button>
+      <button @click="unregisterFoo({ id: k })">Unregister</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { data } from "@/store";
 
 export default Vue.extend({
   name: "HelloWorld",
   props: {
     msg: String,
   },
+  data() {
+    return {
+      id: 0,
+    };
+  },
+  computed: {
+    // typechecker knows what strings are possible here but trying to map a
+    // nested item directly from the root context does not work with the type
+    // checker, e.g., it won't recognize "nested/greeting"
+    ...data.modules.nested.mapper.mapGetters(["greeting"]),
+    foos() {
+      return Object.entries(data.context.state.foo).map(([k, v]) => [
+        k,
+        v.count,
+      ]);
+    },
+  },
+  methods: {
+    ...data.mapper.mapActions(["registerFoo", "unregisterFoo"]),
+    register() {
+      this.id = this.id + 1;
+      this.registerFoo({ id: this.id.toString() });
+    },
+    increment(id: string) {
+      // accessing dynamically registered nested modules doesn't work since
+      // vuex-smart-module doesn't know about them during initialization so we
+      // have to use the special object we created
+      data.modules.foo.modules[id].context.mutations.increment(1);
+    },
+  },
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,169 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import {
+  Getters,
+  Mutations,
+  Actions,
+  Module,
+  Context,
+  createStore,
+  registerModule,
+  unregisterModule,
+  createMapper,
+} from "vuex-smart-module";
+import { ComponentMapper } from "vuex-smart-module/lib/mapper";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+class FooState {
+  count = 1;
+}
+
+class FooGetters extends Getters<FooState> {
+  get double() {
+    return this.state.count * 2;
+  }
+}
+
+class FooMutations extends Mutations<FooState> {
+  increment(payload: number) {
+    this.state.count += payload;
+  }
+}
+
+class FooActions extends Actions<
+  FooState,
+  FooGetters,
+  FooMutations,
+  FooActions
+> {
+  incrementAsync(payload: { amount: number; interval: number }) {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        this.mutations.increment(payload.amount);
+        resolve();
+      }, payload.interval);
+    });
+  }
+}
+
+class FooByIdState {
+  [key: string]: FooState;
+}
+
+const fooById = new Module({ state: FooByIdState });
+
+class NestedState {
+  value = "hello";
+}
+
+class NestedGetters extends Getters<NestedState> {
+  greeting(name: string): string {
+    // You don't get access to root getters so you have to use the special
+    // object to access it directly
+    return this.state.value + ", " + name + data.context.state.punctuation;
+  }
+}
+
+export const nested = new Module({
+  state: NestedState,
+  getters: NestedGetters,
 });
+
+class RootState {
+  punctuation = "!";
+  // Need to declare nested state so the typechecker will know about it
+  // (doesn't get automatically added when passing in the nested modules when
+  // creating a new module)
+  nested!: NestedState;
+  foo!: FooByIdState;
+}
+
+class RootGetters extends Getters<RootState> {}
+
+class RootMutations extends Mutations<RootState> {}
+
+class RootActions extends Actions<
+  RootState,
+  RootGetters,
+  RootMutations,
+  RootActions
+> {
+  registerFoo(payload: { id: string }) {
+    if (data.modules.foo.modules[payload.id]) return;
+    const module = new Module({
+      state: FooState,
+      getters: FooGetters,
+      mutations: FooMutations,
+      actions: FooActions,
+    });
+    registerModule(store, ["foo", payload.id], `foo/${payload.id}`, module);
+    data.modules.foo.modules[payload.id] = {
+      module,
+      mapper: createMapper(module),
+      context: module.context(store),
+    };
+  }
+  unregisterFoo(payload: { id: string }) {
+    if (!data.modules.foo.modules[payload.id]) return;
+    unregisterModule(store, data.modules.foo.modules[payload.id].module);
+    Reflect.deleteProperty(data.modules.foo.modules, payload.id);
+  }
+}
+
+const root = new Module({
+  modules: {
+    nested,
+    foo: fooById,
+  },
+  state: RootState,
+  getters: RootGetters,
+  mutations: RootMutations,
+  actions: RootActions,
+});
+
+const store = createStore(root);
+
+// We need to be able to get access to the module instance if we want to use
+// them in our components. Usually you would just access the `modules` property
+// on the root context, but it doesn't know about dynamically registered ones.
+// To get around that, we use this special object that explicitly sets the type
+// for the modules that may be dynamically registered in the future. This object
+// also just sets up the mapper and context for convenience when importing into
+// components. If we want to keep this structure, we can wrap `registerModule`
+// and `unregisterModule` to auto-build this object.
+export const data = {
+  module: root,
+  // mapper allows you to use `mapGetters`, etc., in your components
+  mapper: createMapper(root),
+  // context gives you direct access to the module
+  context: root.context(store),
+  modules: {
+    nested: {
+      module: nested,
+      mapper: createMapper(nested),
+      context: nested.context(store),
+    },
+    foo: {
+      module: fooById,
+      mapper: createMapper(fooById),
+      context: fooById.context(store),
+      modules: {} as {
+        [key: string]: {
+          module: Module<FooState, FooGetters, FooMutations, FooActions>;
+          mapper: ComponentMapper<
+            FooState,
+            FooGetters,
+            FooMutations,
+            FooActions
+          >;
+          context: Context<
+            Module<FooState, FooGetters, FooMutations, FooActions>
+          >;
+        };
+      },
+    },
+  },
+};
+
+export default store;
